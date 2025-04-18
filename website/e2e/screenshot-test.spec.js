5bdc1a24f6e6ea9583cd420b78f81ebd3026693d
IhorMasechko <igor.masechko007@gmail.com>
2025-04-18 13:43:35 +0300

    Created e2e playwright tests

---
+++ website/e2e/screenshot-test.spec.js
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test("Snapshot for Home Page", async ({ page }) => {
+  await page.setViewportSize({ width: 1280, height: 720 });
+
+  await page.goto(process.env.BASE_URL ?? "http://localhost:3000", {
+    timeout: 60000,
+    waitUntil: "networkidle",
+  });
+
+  await Promise.all([
+    page.waitForLoadState("domcontentloaded"),
+    page.waitForLoadState("networkidle"),
+  ]);
+  await page.evaluate(() => {
+    return document.fonts.ready;
+  });
+
+  const snapshot = await page.screenshot({
+    fullPage: true,
+    timeout: 30000,
+  });
+
+  expect(snapshot).toMatchSnapshot("homepage.png", {
+    maxDiffPixels: 300,
+    threshold: 0.5,
+  });
+});
