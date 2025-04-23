1fa2e70ddb6f7306982258e98330e5690e903b1b
Anton Kovalchuk <79926879+Anton-88@users.noreply.github.com>
2025-04-23 17:21:34 +0300

    [433] Moved prettier config files to website folder (#28)
    Moved prettier config under ./website

--- website/e2e/screenshot-test.spec.js
+++ website/e2e/screenshot-test.spec.js
@@ -4,27 +4,27 @@ import { test, expect } from '@playwright/test';
 test('Snapshot for Home Page', async ({ page }) => {
   await page.setViewportSize({
     width: 1280,
-    height: 720
+    height: 720,
   });
 
   await page.goto(process.env.BASE_URL ?? 'http://localhost:3000', {
     timeout: 60000,
-    waitUntil: 'networkidle'
+    waitUntil: 'networkidle',
   });
 
   await Promise.all([
     page.waitForLoadState('domcontentloaded'),
-    page.waitForLoadState('networkidle')
+    page.waitForLoadState('networkidle'),
   ]);
   await page.evaluate(() => document.fonts.ready);
 
   const snapshot = await page.screenshot({
     fullPage: true,
-    timeout: 30000
+    timeout: 30000,
   });
 
   expect(snapshot).toMatchSnapshot('homepage.png', {
     maxDiffPixels: 300,
-    threshold: 0.5
+    threshold: 0.5,
   });
 });
