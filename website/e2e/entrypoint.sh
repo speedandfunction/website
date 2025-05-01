02e03b9df3ef356441a7cf1400957c17da32b6f2
IhorMasechko <igor.masechko007@gmail.com>
2025-05-01 17:33:37 +0300

    Resolve the artifacts issue

--- website/e2e/entrypoint.sh
+++ website/e2e/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/wait-for-it.sh apostrophe:3000 -t 60 -- npx playwright test "$@" 
+/wait-for-it.sh apostrophe:3000 -t 60 -- sh -c "wget -qO- http://apostrophe:3000/ > /dev/null && npx playwright test \"$@\"" 
