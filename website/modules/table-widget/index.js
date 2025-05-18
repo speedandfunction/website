994ad923e69ff193819eb19fee97e03529cc6215
Anton Kovalchuck <antonkovalchuck@gmail.com>
2025-05-18 17:44:27 +0300

    fixed sonarqube issue

--- website/modules/table-widget/index.js
+++ website/modules/table-widget/index.js
@@ -1,5 +1,6 @@
 const extendedToolbar = require('../../lib/extendedToolbar');
 const headingToolbar = require('../../lib/headingToolbar');
+const linkSchema = require('../../lib/linkSchema');
 
 module.exports = {
   extend: '@apostrophecms/widget-type',
@@ -46,70 +47,7 @@ module.exports = {
               },
               required: true,
             },
-            linkTitle: {
-              label: 'Link Title',
-              type: 'string',
-            },
-            linkType: {
-              label: 'Link type',
-              type: 'select',
-              required: false,
-              choices: [
-                {
-                  label: 'Page',
-                  value: 'page',
-                },
-                {
-                  label: 'File',
-                  value: 'file',
-                },
-                {
-                  label: 'Custom URL',
-                  value: 'custom',
-                },
-              ],
-            },
-            _page: {
-              label: 'Link to page',
-              type: 'relationship',
-              withType: '@apostrophecms/page',
-              max: 1,
-              builders: {
-                project: {
-                  title: 1,
-                  _url: 1,
-                },
-              },
-              if: {
-                linkType: 'page',
-              },
-            },
-            _file: {
-              label: 'Link to file',
-              type: 'relationship',
-              withType: '@apostrophecms/file',
-              max: 1,
-              if: {
-                linkType: 'file',
-              },
-            },
-            customUrl: {
-              label: 'URL for custom link',
-              type: 'url',
-              if: {
-                linkType: 'custom',
-              },
-            },
-            target: {
-              label: 'Will the link open a new browser tab?',
-              type: 'checkboxes',
-              choices: [
-                {
-                  label: 'Open in a new tab',
-                  value: '_blank',
-                },
-              ],
-            },
+            ...linkSchema.fields.add,
           },
         },
       },
