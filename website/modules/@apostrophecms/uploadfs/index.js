fe2b49766f4741d5a0d938cc86c50d1481fa9115
Peter Ovchyn <peter@boarlabs.xyz>
2025-05-19 22:42:22 +0200

    Fix S3 configuration for uploadfs

--- website/modules/@apostrophecms/uploadfs/index.js
+++ website/modules/@apostrophecms/uploadfs/index.js
@@ -13,7 +13,7 @@ const res = {
       region: getEnv('APOS_S3_REGION'),
       endpoint: getEnv('APOS_S3_ENDPOINT'),
       style: getEnv('APOS_S3_STYLE'),
-      https: false,
+      https: getEnv('APOS_S3_HTTPS'),
       cdn: {
         enabled: true,
         url: getEnv('APOS_CDN_URL'),
