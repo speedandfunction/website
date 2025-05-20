a5ab2bfd3dab76f33db1e7d1bb132c082424c928
Anton Kovalchuck <antonkovalchuck@gmail.com>
2025-05-20 22:20:14 +0300

    Removed unnecessary category type

--- website/modules/categories/index.js
+++ website/modules/categories/index.js
@@ -10,44 +10,11 @@ module.exports = {
         type: 'string',
         label: 'Description',
       },
-      categoryType: {
-        type: 'select',
-        label: 'Category Type',
-        required: true,
-        choices: [
-          {
-            label: 'Technology',
-            value: 'Technology',
-          },
-          {
-            label: 'Case Study Type',
-            value: 'Case Study Type',
-          },
-          {
-            label: 'Industry',
-            value: 'Industry',
-          },
-        ],
-      },
     },
     group: {
       basics: {
         label: 'Basics',
-        fields: ['title', 'description', 'categoryType'],
-      },
-    },
-  },
-  columns: {
-    add: {
-      categoryType: {
-        label: 'Category Type',
-      },
-    },
-  },
-  filters: {
-    add: {
-      categoryType: {
-        label: 'Category Type',
+        fields: ['title', 'description'],
       },
     },
   },
