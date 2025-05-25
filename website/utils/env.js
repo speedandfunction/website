0f84874b2dec1603d3c872edabe1885a2ce7d4ed
Vitalii Pikozh <wisart@gmail.com>
2025-05-25 14:50:29 +0300

    improve env variable validation

--- website/utils/env.js
+++ website/utils/env.js
@@ -1,7 +1,7 @@
 // Environment helper utility
 const getEnv = (name) => {
   const value = process.env[name];
-  if (value === undefined) {
+  if (value === undefined || value === '') {
     throw new Error(`Environment variable "${name}" is not defined`);
   }
   return value;
