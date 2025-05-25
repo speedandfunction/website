a48fa363de9a430f7989d9c3d91643f6f4fdf5c2
Vitalii Pikozh <wisart@gmail.com>
2025-05-25 17:26:03 +0300

    fix getEnv

--- website/utils/env.js
+++ website/utils/env.js
@@ -1,7 +1,7 @@
 // Environment helper utility
 const getEnv = (name) => {
   const value = process.env[name];
-  if (value === undefined || value === '') {
+  if (value === undefined) {
     throw new Error(`Environment variable "${name}" is not defined`);
   }
   return value;
