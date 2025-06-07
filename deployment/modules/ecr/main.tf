3b5bac3908a8a65ff36df5a3d08435d6e477539a
Peter Ovchyn <peter@boarlabs.xyz>
2025-06-07 21:24:43 +0200

    Add force delete option to ECR repository
    - Enable force_delete on ECR repository resource to allow deletion with images

--- deployment/modules/ecr/main.tf
+++ deployment/modules/ecr/main.tf
@@ -2,6 +2,7 @@
 resource "aws_ecr_repository" "main" {
   name                 = "${var.name_prefix}-${var.environment}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
