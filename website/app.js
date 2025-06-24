aaee6b3cbe7c30ff3470bf79eefe8df81c85bb10
Yuriy <yuramax@gmail.com>
2025-06-24 10:57:25 +0300

    Fix shared-constants module registration in app.js

--- website/app.js
+++ website/app.js
@@ -48,7 +48,7 @@ function createAposConfig() {
       'global-data': {},
 
       // Shared constants module
-      'shared-constants': {},
+      '@apostrophecms/shared-constants': {},
 
       // Configure page types
       '@apostrophecms/rich-text-widget': {},
