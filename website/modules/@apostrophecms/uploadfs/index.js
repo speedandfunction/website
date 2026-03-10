a7a60566cf3d762d3bc990bdd612b8082b353ea1
Ihor Masechko <ihormasechko@Ihors-MacBook-Air.local>
2026-03-10 13:34:17 +0200

    fix(uploadfs): normalize S3 https env flag

--- website/modules/@apostrophecms/uploadfs/index.js
+++ website/modules/@apostrophecms/uploadfs/index.js
@@ -9,7 +9,7 @@ const uploadfsOptions = {
   region: getEnv('APOS_S3_REGION'),
   endpoint: getEnv('APOS_S3_ENDPOINT'),
   style: getEnv('APOS_S3_STYLE'),
-  https: aposS3Https === 'true',
+  https: aposS3Https.toLowerCase() === 'true',
   cdn: {
     enabled: true,
     url: getEnv('APOS_CDN_URL'),
