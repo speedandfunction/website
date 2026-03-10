e77392b6f08eaadbe539f9f7d4a9cb023ca254a2
Ihor Masechko <ihormasechko@Ihors-MacBook-Air.local>
2026-03-10 13:26:02 +0200

    fix(uploadfs): correctly resolve S3 https flag and credentials

--- website/modules/@apostrophecms/uploadfs/index.js
+++ website/modules/@apostrophecms/uploadfs/index.js
@@ -1,6 +1,7 @@
 const { getEnv } = require('../../../utils/env');
 
 const aposS3Secret = process.env.APOS_S3_SECRET;
+const aposS3Https = getEnv('APOS_S3_HTTPS');
 
 const uploadfsOptions = {
   storage: 's3',
@@ -8,14 +9,14 @@ const uploadfsOptions = {
   region: getEnv('APOS_S3_REGION'),
   endpoint: getEnv('APOS_S3_ENDPOINT'),
   style: getEnv('APOS_S3_STYLE'),
-  https: getEnv('APOS_S3_HTTPS'),
+  https: aposS3Https === 'true',
   cdn: {
     enabled: true,
     url: getEnv('APOS_CDN_URL'),
   },
 };
 
-if (aposS3Secret !== undefined) {
+if (aposS3Secret) {
   uploadfsOptions.secret = aposS3Secret;
   uploadfsOptions.key = getEnv('APOS_S3_KEY');
 }
