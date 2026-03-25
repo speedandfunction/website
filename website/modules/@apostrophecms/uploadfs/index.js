f66727f59b88221f6308cac41aeb0f7843c0fc96
Ihor Masechko <ihormasechko@Ihors-MacBook-Air.local>
2026-03-25 23:45:24 +0200

    fix(uploadfs): set S3 bucketObjectsACL to private by default

--- website/modules/@apostrophecms/uploadfs/index.js
+++ website/modules/@apostrophecms/uploadfs/index.js
@@ -9,7 +9,6 @@ const uploadfsOptions = {
   storage: 's3',
   bucket: getEnv('APOS_S3_BUCKET'),
   region: getEnv('APOS_S3_REGION'),
-  acl: 'private',
   cdn: {
     enabled: true,
     url: getEnv('APOS_CDN_URL'),
@@ -32,6 +31,11 @@ if (aposS3Secret) {
   uploadfsOptions.secret = aposS3Secret;
   uploadfsOptions.key = getEnv('APOS_S3_KEY');
 }
+
+const aposS3BucketObjectsAcl =
+  process.env.APOS_S3_ACL || 'private';
+uploadfsOptions.bucketObjectsACL = aposS3BucketObjectsAcl;
+uploadfsOptions.disabledBucketObjectsACL = aposS3BucketObjectsAcl;
 
 const res = {
   options: {
