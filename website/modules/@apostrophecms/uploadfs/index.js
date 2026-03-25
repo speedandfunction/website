8a81f8200da6d110fe19754635ae480f96597260
Ihor Masechko <ihormasechko@Ihors-MacBook-Air.local>
2026-03-26 00:02:30 +0200

    fix lint error

--- website/modules/@apostrophecms/uploadfs/index.js
+++ website/modules/@apostrophecms/uploadfs/index.js
@@ -32,8 +32,7 @@ if (aposS3Secret) {
   uploadfsOptions.key = getEnv('APOS_S3_KEY');
 }
 
-const aposS3BucketObjectsAcl =
-  process.env.APOS_S3_ACL || 'private';
+const aposS3BucketObjectsAcl = process.env.APOS_S3_ACL || 'private';
 uploadfsOptions.bucketObjectsACL = aposS3BucketObjectsAcl;
 uploadfsOptions.disabledBucketObjectsACL = aposS3BucketObjectsAcl;
 
