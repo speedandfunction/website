a4b0bacc4fad61817dd42636e3a60f8e38142e57
sf-alexo <87033189+sf-alexo@users.noreply.github.com>
2026-09-18 15:14:54 +0200

    page/lets-talk (#297)
    Co-authored-by: Alex Ost <alex.ost@speedandfunction.com>

--- website/modules/contact-widget/index.js
+++ website/modules/contact-widget/index.js
@@ -6,6 +6,11 @@ module.exports = {
   },
   fields: {
     add: {
+      heading: {
+        label: 'Heading',
+        type: 'string',
+        help: 'Optional heading displayed above the form, e.g. "Let\'s talk".',
+      },
       form: {
         label: 'Form',
         type: 'area',
